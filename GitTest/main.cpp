8b09355d28ac94d8dad03995ffc4e2f6cc28839b
mgeorgiev <mityo.g.georgiev@gmail.com>
2017-12-26 17:44:11 +0200

    Initial Commit

---
+++ GitTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GitTest
+//
+//  Created by m.georgiev on 26.12.17.
+//  Copyright © 2017 m.georgiev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
